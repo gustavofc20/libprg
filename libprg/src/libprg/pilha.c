2f68dc2894f0ce78f7281b2ed1b082e4f8010da0
Gustavofc20 <Gustavo.fc31@aluno.ifsc.edu.br>
2025-09-02 11:22:25 -0300

    tentativas de emplementação do "empilhar", "desempilhar" e "tamanho".

--- libprg/src/libprg/pilha.c
+++ libprg/src/libprg/pilha.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 typedef struct  pilha {
-    int* elemnetos;
+    int* elementos;
     int topo;
     int capacidade;
 }pilha_t;
@@ -11,21 +11,34 @@ pilha_t* criar_pilha(int cap) {
 
     pilha_t* p = malloc(sizeof(pilha_t));
 
-    p->elemnetos = malloc(cap*sizeof(int));
+    p->elementos = malloc(cap*sizeof(int));
     p->topo = -1;
-    p->capacidade= cap;
+    p->capacidade = cap;
 
     return p;
 }
 //empilhar
-int empilhar(pilha_t* p, int valor) {
+pilha_t* empilhar(pilha_t* p, int valor) {
 
     p->topo++;
-    p->elemnetos[p->topo]=valor;
+    p->elementos[p->topo] = valor;
 
     return 0;
 }
 //desempilhar
+pilha_t* desempilhar(pilha_t* p) {
+
+    p->elementos[p->topo];
+    p->topo--;
+
+    return 0;
+}
 //tamanho
+pilha_t* tamanho(pilha_t* p) {
+
+    p->topo++;
+
+    return p;
+}
 //vazia
 //destruir
