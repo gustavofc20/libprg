9537ffe2a7e737456e3ee52142be8857c4d014ec
Gustavofc20 <Gustavo.fc31@aluno.ifsc.edu.br>
2025-09-02 10:32:23 -0300

    configurações para iniciar o laboratorio 2.

---
+++ libprg/src/libprg/pilha.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "header/lab_2.h"
+#include <stdlib.h>
+
+typedef struct  pilha {
+    int* elemnetos;
+    int topo;
+    int capacidade;
+}pilha_t;
+
+pilha_t* criar_pilha(int cap) {
+
+    pilha_t* p = malloc(sizeof(pilha_t));
+
+    p->elemnetos = malloc(cap*sizeof(int));
+    p->topo = -1;
+    p->capacidade= cap;
+
+    return p;
+}
+//empilhar
+//desempilhar
+//tamanho
+//vazia
+//destruir
