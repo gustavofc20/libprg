300c14cf18fc1900a9cf5cdca503eb09a03f4552
Gustavo.fc31 <Gustavo.fc31@aluno.ifsc.edu.br>
2025-09-30 08:50:33 -0300

    inicialização do lista_encadeada, seguindo a explicação do prefessor

--- libprg/src/libprg/lista_encadeada.c
+++ libprg/src/libprg/lista_encadeada.c
@@ -1,10 +1,34 @@
-//
-// Created by aluno on 25/09/2025.
-//
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "libprg/libprg.h"
 
+typedef struct no {
+    int valor;
+    struct no* proximo;
+} no_t;
 
 //criar
+
+no_t* criar_lista_encadeada(int valor) {
+
+    no_t* no = malloc(sizeof(no_t));
+    no->valor = valor;
+    no->proximo = NULL;
+
+    return no;
+}
+
 //adicinar
+
+no_t* adicionar(no_t* inicio, int valor) {
+
+    no_t* novo_no = criar_lista_encadeada(valor);
+    novo_no->proximo=inicio;
+    inicio=novo_no;
+
+    return inicio;
+}
 //remover
 //buscar
 //destruir
