13f428508aa3d8121e2473f8e00842ec3d1cb559
Gustavo.fc31 <Gustavo.fc31@aluno.ifsc.edu.br>
2026-03-26 09:36:47 -0300

    finalização da pilha.

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -8,7 +8,12 @@ typedef struct pilha pilha_t;
 pilha_t* pilha_cria(int capacidade);
 void pilha_libera(pilha_t* pilha);
 
+int pilha_cheia(pilha_t *pilha);
+int pilha_vazia(pilha_t *pilha);
+
 int empilhar(pilha_t* pilha, int valor);
-void desempilhar(pilha_t* pilha);
+int desempilhar(pilha_t *pilha);
+int topo_pilha(pilha_t *pilha);
+int tamanho_pilha(pilha_t *pilha);
 
 #endif
--- libprg/src/libprg/pilha.c
+++ libprg/src/libprg/pilha.c
@@ -20,6 +20,18 @@ pilha_t* pilha_cria(int capacidade) {
     return pilha;
 }
 
+int pilha_cheia(pilha_t *pilha) {
+    if (pilha->capacidade >= pilha->topo)
+        return 1;
+    return 0;
+}
+
+int pilha_vazia(pilha_t *pilha) {
+    if (pilha->topo < 0)
+        return 1;
+    return 0;
+}
+
 void pilha_libera(pilha_t* pilha) {
     free(pilha->elementos);
     free(pilha);
@@ -27,24 +39,33 @@ void pilha_libera(pilha_t* pilha) {
 
 int empilhar(pilha_t* pilha, int valor) {
 
-    if (pilha->topo == pilha->capacidade - 1) {
-        return -1;
-    }
+    if (pilha_cheia(pilha) == 1) {
+        pilha->capacidade *= 2;
+        pilha->elementos = realloc(pilha->elementos,pilha->capacidade * sizeof(int));
 
+        if (pilha->elementos == NULL)
+            return -1;
+    }
     pilha->topo++;
     pilha->elementos[pilha->topo] = valor;
 
-    return pilha->elementos[pilha->topo - 1];
+    return 0;
 }
 
-void desempilhar(pilha_t* pilha) {
-    if (pilha->topo == -1) {
-        return;
-    }
+int desempilhar(pilha_t *pilha) {
+    if (pilha_cheia(pilha) == 1)
+        return -1;
 
+    int valor = pilha->elementos[pilha->topo];
     pilha->topo--;
+
+    return valor;
 }
 
-void pilha_imprime(pilha_t* pilha) {
+int topo_pilha(pilha_t *pilha) {
+    return pilha->topo;
+}
 
+int tamanho_pilha(pilha_t *pilha) {
+    return pilha->topo+1;
 }
